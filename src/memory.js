c78225305b73a59e63b2dc5f1f3d95a694177665
Adrian <adrianbueno095@gmail.com>
2023-11-30 14:52:36 +0100

    fix bug

--- src/memory.js
+++ src/memory.js
@@ -7,6 +7,7 @@ class MemoryGame {
   }
 
   shuffleCards() {
+    if (!this.cards) { return undefined };
     for (let i = this.cards.length - 1; i > 0; i--) {
       const randomIndex = Math.floor(Math.random() * (i + 1));
       [this.cards[i], this.cards[randomIndex]] = [this.cards[randomIndex], this.cards[i]];
